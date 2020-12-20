8afffde7d12be1eb25271add253c3cab83f589e6
Karen Ardila <karen.ardila1893@gmail.com>
2020-12-19 19:19:21 -0600

    update sort

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,10 +9,10 @@ const Home = () => {
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/pets`)
             .then(res => setPets(res.data.pets.sort((a, b) => {
-                if (a.type < b.type) return -1;
-                if (a.type > b.type) return 1;
-                return 0;
-              })))
+                var textA = a.type.toUpperCase();
+                var textB = b.type.toUpperCase();
+                return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+            })))
     }, [])
 
     return (
@@ -28,7 +28,7 @@ const Home = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {pets.map((pet, i) => <tr key={i} className={i % 2 === 0 ? 'table-row': 'table-row-odd'}>
+                    {pets.map((pet, i) => <tr key={i} className={i % 2 === 0 ? 'table-row' : 'table-row-odd'}>
                         <td>{pet.name}</td>
                         <td>{pet.type}</td>
                         <td><Link to={`/pets/${pet._id}`}>details</Link> | <Link to={`/pets/${pet._id}/edit`}>edit</Link></td>
